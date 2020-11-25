4ff70665dedbaf98cd9a37446f6493502a5f1f2e
EricVoll <eric.vollenweider@gmail.com>
2020-11-25 11:05:27 +0100

    Extract asa ros mocking

---
+++ scripts/asa_mocker.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+
+## Simple talker demo that listens to std_msgs/Strings published 
+## to the 'chatter' topic
+import rospy
+import tf2_ros as tf
+from std_msgs.msg import String
+from geometry_msgs.msg import TransformStamped, Quaternion
+from nav_msgs.msg import Odometry
+from asa_ros_commander.msg import CreatedAnchorMock, FoundAnchorMock, AsaRelPoseStamped
+import time
+
+class AsaAnchorMocker:
+
+
+    def __init__(self):
+        rospy.init_node('asa_anchor_mocker', anonymous=True)
+        self.tf_broadcaster = tf.StaticTransformBroadcaster()
+        self.tf_Buffer = tf.Buffer(rospy.Duration(10))
+        self.rate = rospy.Rate(10.0)
+        self.anchors = []
+        self.found_anchor_pub = rospy.Publisher('found_anchor', FoundAnchorMock, queue_size=10)
+        self.created_anchor_pub = rospy.Publisher('created_anchor', CreatedAnchorMock, queue_size=10)
+        self.world_frame_id = "odom"
+
+        #add all subscribers needed
+        rospy.Subscriber('/mock_anchor_found', AsaRelPoseStamped, self.mock_anchor_callback_found)
+        rospy.Subscriber('/mock_anchor_created', AsaRelPoseStamped, self.mock_anchor_callback_created)
+
+        rospy.loginfo("Initialized asa_mocker")       
+
+    def spin(self):
+        rospy.spin()
+
+    # transforms a post to a transform object
+    def pose_to_tf(self, pose, frame_name, parent_frame, time=None):
+        """
+        Generate a TF from a given pose, frame, and parent.
+        """
+        assert pose is not None, 'Cannot have None for pose.'
+        _tf = TransformStamped()
+        
+        _tf.child_frame_id = frame_name
+        if time is None:
+            time = rospy.Time.now()
+        _tf.header.stamp = time
+        _tf.header.frame_id = parent_frame
+
+        _tf.transform.translation = pose.position
+        _tf.transform.rotation = pose.orientation
+
+        return _tf
+
+    # Creates a tf frame at the defined location, mocks the asa ros wrappers published msg
+    def mock_anchor_callback_created(self, data):
+        rospy.loginfo(rospy.get_caller_id() + ' Mocking anchor created '+ data.anchor_id  +' at location %s', data.pose)
+        transform = self.pose_to_tf(data.pose, data.anchor_id, self.world_frame_id, rospy.Time.now())
+        self.tf_broadcaster.sendTransform(transform)
+
+        newAnchor = CreatedAnchorMock()
+        newAnchor.success = True
+        newAnchor.anchor_id = data.anchor_id
+        newAnchor.failure_reason = ""
+
+        self.created_anchor_pub.publish(newAnchor)
+
+        
+    # Creates a tf frame at the defined location, mocks the asa ros wrappers published msg
+    def mock_anchor_callback_found(self, data):
+        rospy.loginfo(rospy.get_caller_id() + ' Mocking anchor ound '+ data.anchor_id  +' at location %s', data.pose)
+        transform = self.pose_to_tf(data.pose, data.anchor_id, self.world_frame_id, rospy.Time.now())
+        self.tf_broadcaster.sendTransform(transform)
+
+        newAnchor = FoundAnchorMock()
+        newAnchor.anchor_id = data.anchor_id
+
+        self.found_anchor_pub.publish(newAnchor)
+
+
+
+if __name__ == '__main__':
+    rospy.loginfo("Starting asa_mocker")
+    mocker = AsaAnchorMocker()
+    mocker.spin()
--- scripts/commander.py
+++ scripts/commander.py
@@ -9,6 +9,8 @@ from std_msgs.msg import String
 from geometry_msgs.msg import TransformStamped, Quaternion
 from nav_msgs.msg import Odometry
 from asa_ros_commander.msg import AsaRelPoseStamped
+from asa_ros_commander.msg import CreatedAnchorMock as CreatedAnchor
+from asa_ros_commander.msg import FoundAnchorMock as FoundAnchor
 import time
 
 class AsaCommander:
@@ -16,57 +18,51 @@ class AsaCommander:
 
     def __init__(self):
         rospy.init_node('asa_commander', anonymous=True)
-        self.tf_broadcaster = tf.StaticTransformBroadcaster()
-        self.tf_Buffer = tf.Buffer(rospy.Duration(120))
+        self.tf_Buffer = tf.Buffer(rospy.Duration(10))
         self.tf_listener = tf.TransformListener(self.tf_Buffer)
         self.odom_publisher = rospy.Publisher('/odometry/filtered/asa_relative', Odometry, queue_size=10)
         self.current_anchor_id = "odom"
         self.rate = rospy.Rate(10.0)
         self.anchors = []
+        self.robot_frame = "base_link"
 
         #add all subscribers needed
-        rospy.Subscriber('/mock_anchor', AsaRelPoseStamped, self.mock_anchor_callback)
         rospy.Subscriber('/anchored_goal', AsaRelPoseStamped, self.anchored_goal_callback)
         rospy.Subscriber('/odometry/filtered', Odometry, self.republish_robot_pos)
+
+        # Subscribers listening to the asa ros wrapper
+        rospy.Subscriber('/found_anchor', FoundAnchor, self.asa_found_anchor_callback)
+        rospy.Subscriber('/created_anchor', CreatedAnchor, self.asa_created_anchor_callback)
 
         rospy.loginfo("Initialized asa_ros_commander")       
 
     def spin(self):
         rospy.spin()
 
-    # transforms a post to a transform object
-    def pose_to_tf(self, pose, frame_name, parent_frame, time=None):
-        """
-        Generate a TF from a given pose, frame, and parent.
-        """
-        assert pose is not None, 'Cannot have None for pose.'
-        _tf = TransformStamped()
-        
-        _tf.child_frame_id = frame_name
-        if time is None:
-            time = rospy.Time.now()
-        _tf.header.stamp = time
-        _tf.header.frame_id = parent_frame
+#########
+# Utils #
+#########
 
-        _tf.transform.translation = pose.position
-        _tf.transform.rotation = pose.orientation
 
-        return _tf
+#####################
+# Anchor management #
+#####################
 
-    def anchored_goal_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + ' Commanding robot relative to the anchor with id %s', data.anchor_id)
+    # Adds the found anchors id to the available id list
+    def asa_found_anchor_callback(self, data):
+        self.add_asa_frame(data.anchor_id)
 
-    # Creates an anchor at the defined location relative to odom
-    def mock_anchor_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + ' received msg')
-        rospy.loginfo(rospy.get_caller_id() + ' Mocking anchor '+ data.anchor_id  +' at location %s', data.pose)
-        transform = self.pose_to_tf(data.pose, data.anchor_id, "odom", rospy.Time.now())
-        self.tf_broadcaster.sendTransform(transform)
-        self.set_current_asa_frame(data.anchor_id)
+    # Adds the created anchors id to the available id list if it succeeded
+    def asa_created_anchor_callback(self, data):
+        if(data.success):
+            self.add_asa_frame(data.anchor_id)
+        else:
+            rospy.loginfo("Asa Anchor Creation failed")
+
 
 
     # Adds an anchor to the anchor list and overwrites the default if none is set
-    def set_current_asa_frame(self, anchor_id):
+    def add_asa_frame(self, anchor_id):
         #overwrite the default the first time we receive an anchor
         if self.current_anchor_id == "odom":
             self.current_anchor_id = anchor_id
@@ -74,13 +70,44 @@ class AsaCommander:
             self.anchors.append(anchor_id)
             rospy.loginfo("added id " + anchor_id + " to anchor list")
 
+
+    # Queries all registered anchors and returns the id of the nearest one to the robot frame
+    def find_nearest_anchor(self):
+
+        if(len(self.anchors) == 1):
+            return self.anchors[0]
+
+        smallestDistance = 1000000
+        smallestDistanceAnchorId = None
+        for anchor in self.anchors:
+            distance = self.getDistanceToAnchor(anchor, self.robot_frame)
+            if(distance < smallestDistance):
+                smallestDistance = distance
+                smallestDistanceAnchorId = anchor
+
+        return smallestDistanceAnchorId
+
+    # returns the distance between the two frames
+    def getDistanceToAnchor(self, anchor_id, target_frame):
+        trans = self.tf_Buffer.lookup_transform(target_frame, anchor_id, rospy.Time(0))
+        tr = trans.transform.translation
+        return (tr.x**2 + tr.y**2 + tr.z**2)**0.5
+
+################
+# Republishers #
+################
+
+    def anchored_goal_callback(self, data):
+        rospy.loginfo(rospy.get_caller_id() + ' Commanding robot relative to the anchor with id %s', data.anchor_id)
+
+
     # Republishes the received omodemetry relative to the odom frame but relative to the nearest anchor available
     def republish_robot_pos(self, data):
         current_parent_frame_id = data.header.frame_id
         child_frame_id = data.child_frame_id
 
         if(current_parent_frame_id == self.current_anchor_id):
-            rospy.loginfo("Skipped republishing since current anchor id "+ self.current_anchor_id + " is same as sent parent")
+            #rospy.loginfo("Skipped republishing since current anchor id "+ self.current_anchor_id + " is same as sent parent")
             return
 
         self.current_anchor_id = self.find_nearest_anchor()
@@ -108,28 +135,6 @@ class AsaCommander:
 
         self.odom_publisher.publish(new_odom)
 
-    # finds the nearest anchor to the base_link frame
-    def find_nearest_anchor(self):
-
-        if(len(self.anchors) == 1):
-            return self.anchors[0]
-
-        smallestDistance = 1000000
-        smallestDistanceAnchor = None
-        for anchor in self.anchors:
-            distance = self.getDistanceToAnchor(anchor, "base_link")
-            if(distance < smallestDistance):
-                smallestDistance = distance
-                smallestDistanceAnchor = anchor
-
-        rospy.loginfo("Smallest distance is: %s" , smallestDistance)
-        return smallestDistanceAnchor
-
-    # returns the distance between the two frames
-    def getDistanceToAnchor(self, anchor_id, target_frame):
-        trans = self.tf_Buffer.lookup_transform(target_frame, anchor_id, rospy.Time(0))
-        tr = trans.transform.translation
-        return (tr.x**2 + tr.y**2 + tr.z**2)**0.5
 
 if __name__ == '__main__':
     commander = AsaCommander()
