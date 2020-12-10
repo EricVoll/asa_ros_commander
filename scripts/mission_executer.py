9ea662ef5fb3a6d4a93458265754f4eec51fd176
EricVoll <eric.vollenweider@gmail.com>
2020-12-10 13:12:18 +0100

    Find all anchors on startup

--- scripts/mission_executer.py
+++ scripts/mission_executer.py
@@ -33,7 +33,8 @@ class Mission(object):
         self.__dict__.update(json.loads(jsonString))
 
     def get_anchor_ids(self):
-        anchor_ids = [x.Id for x in self.SpatialAnchors]
+        rospy.loginfo(self.SpatialAnchors[0])
+        anchor_ids = [x["Id"] for x in self.SpatialAnchors]
         return anchor_ids
 
 class ASAHandler:
@@ -44,8 +45,7 @@ class ASAHandler:
 
     def find_anchor(self, anchor_id):
         rospy.loginfo("Trying to find anchor with id " + anchor_id)
-        msg = "anchor_id: '" + anchor_id + "'"
-        response = self.trajectory_service(String(msg))
+        response = self.find_anchor_service(anchor_id)
         rospy.loginfo(response)
 
     def find_anchors(self, anchor_ids):
@@ -63,7 +63,7 @@ class MissionExecuter:
         self.mission = None
         self.asa_handler = ASAHandler()
 
-        jsonString = '{"$id":"1","SpatialAnchors":[{"$id":"2","IsRoot":false,"Id":"id","RelativePoseToParent":{"$id":"3","X":0.0,"Y":0.0,"Z":0.0,"i":0.0,"j":0.0,"k":0.0,"w":1.0},"ParentAnchorId":"root"},{"$id":"4","IsRoot":false,"Id":"id","RelativePoseToParent":{"$id":"5","X":0.0,"Y":0.0,"Z":0.0,"i":0.0,"j":0.0,"k":0.0,"w":1.0},"ParentAnchorId":"root"}],"Waypoints":[{"$id":"6","$type":"WaypointControl.Waypoint.Waypoint, WaypointControl","Name":"EntryPoint","Anchor":{"$ref":"2"},"Pose":{"$id":"7","$type":"RobotUtilities.Pose, RobotUtilities","X":0.0,"Y":0.0,"Z":0.0,"i":0.0,"j":0.0,"k":0.0,"w":1.0},"LifetimeId":"bf5afe04-31b8-438e-9f9f-7c040b52bb48","TargetSetTasks":[],"EntryTasks":[],"MainTasks":[],"ExitTasks":[]},{"$id":"8","$type":"WaypointControl.Waypoint.Waypoint, WaypointControl","Name":"wp1","Anchor":{"$ref":"4"},"Pose":{"$id":"9","$type":"RobotUtilities.Pose, RobotUtilities","X":0.0,"Y":0.0,"Z":0.0,"i":0.0,"j":0.0,"k":0.0,"w":1.0},"LifetimeId":"2fd81f40-0157-4d45-88ee-8dd43ea9374d","TargetSetTasks":[],"EntryTasks":[],"MainTasks":[],"ExitTasks":[]}],"Edges":[{"$id":"10","$type":"WaypointControl.Edge.Edge, WaypointControl","Source":{"$ref":"6"},"Target":{"$ref":"8"}}],"EntryPoint":{"$ref":"6"}}'
+        jsonString = '{"$id":"1","SpatialAnchors":[{"$id":"4","IsRoot":true,"Id":"2da468a2-cf0a-4435-9379-cd4016659061","RelativePoseToParent":{"$id":"5","X":0,"Y":0,"Z":0,"i":0,"j":0,"k":0,"w":1},"ParentAnchorId":"root"}],"Waypoints":[{"$id":"6","$type":"WaypointControl.Waypoint.Waypoint, WaypointControl","Name":"EntryPoint","Anchor":{"$ref":"4"},"Pose":{"$id":"7","$type":"RobotUtilities.Pose, RobotUtilities","X":0,"Y":0,"Z":0,"i":0,"j":0,"k":0,"w":1},"LifetimeId":"WP1","TargetSetTasks":[],"EntryTasks":[],"MainTasks":[],"ExitTasks":[]},{"$id":"8","$type":"WaypointControl.Waypoint.Waypoint, WaypointControl","Name":"wp1","Anchor":{"$ref":"4"},"Pose":{"$id":"9","$type":"RobotUtilities.Pose, RobotUtilities","X":1.5,"Y":0,"Z":0,"i":0,"j":0,"k":0,"w":1},"LifetimeId":"WP2","TargetSetTasks":[],"EntryTasks":[],"MainTasks":[],"ExitTasks":[]}],"Edges":[{"$id":"10","$type":"WaypointControl.Edge.Edge, WaypointControl","Source":{"$ref":"6"},"Target":{"$ref":"8"}}],"EntryPoint":{"$ref":"6"}}'
 
         self.load_mission(jsonString)
 
